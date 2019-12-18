7672d4ab5b0602e69b52790d928122742748a51d
BhagathReddy-K <bkatta@accela.com>
2019-12-18 23:42:17 +0530

    setup connection bitween stock user and api frontend

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,15 @@
 var app = angular.module('FinanceTrackerApp', [])
+		         .controller('stocksController', ['$scope', function($scope){
+		         	$scope.stock = {};
+		         	$scope.lookup = function(){
+		         	 if($scope.ticker != undefined && $scope.ticker != '') {
+		         		$scope.stock = {
+		         			symbol: 'Foo',
+		         			name: 'Example Corp.',
+		         			last_price: '123.00'
+		         		}
+		         	  } else {
+		         		$scope.stock = {}
+		         		}	
+		         	}
+		         }])
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,7 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
+  attr_accessor :can_be_added
 
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
@@ -8,7 +9,7 @@ class Stock < ApplicationRecord
 
   def self.new_from_lookup(ticker_symbol)
     begin
-      client = IEX::Api::Client.new(publishable_token: 'PUBLISHABLE TOKEN HERE')
+      client = IEX::Api::Client.new(publishable_token: 'pk_d4a69538fa5446c9b9296686df0d4699')
       looked_up_stock = client.quote(ticker_symbol)
       new(name: looked_up_stock.company_name,
           ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
