129fb346c66dc0aa1aad04df1eec3a40286aa067
BhagathReddy-K <bkatta@accela.com>
2019-12-20 21:25:22 +0530

    stock list display dynamic

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,15 +1,55 @@
 var app = angular.module('FinanceTrackerApp', [])
-		         .controller('stocksController', ['$scope', function($scope){
+				.factory('stockService', ['$http', function($http){
+		         		var stockApi = {};
+		         		stockApi.searchStocks = function(symbol){
+		         			return $http.get('/search_stocks.json?stock=' + symbol);
+		         		}
+		         		stockApi.addStockToPortfolio = function(symbol) {
+		         			return $http.post('/user_stocks.json?stock=' +symbol);
+		         		}
+		         		return stockApi;
+		         	}])
+		         .controller('stocksController', ['$scope', 'stockService', function($scope, stockService){
+
 		         	$scope.stock = {};
+
 		         	$scope.lookup = function(){
 		         	 if($scope.ticker != undefined && $scope.ticker != '') {
-		         		$scope.stock = {
-		         			symbol: 'Foo',
-		         			name: 'Example Corp.',
-		         			last_price: '123.00'
-		         		}
-		         	  } else {
+		         	 	stockService.searchStocks($scope.ticker)
+		         	    .then(function(response){
+		         	    	$scope.stock.error      = null;
+		         	    	$scope.stock.message    = null;
+		         	    	$scope.stock.symbol     = response.data.ticker;
+		         	    	$scope.stock.name       = response.data.name;
+		         	    	$scope.stock.last_price = response.data.last_price;
+		         	    	$scope.stock.can_be_added = response.data.can_be_added;
+		         	    },
+		         	     function(response){
+		         	     	$scope.stock = {};
+		         	     	$scope.stock.error = response.data.response;
+		         	     });
+
+		        	  } else {
 		         		$scope.stock = {}
+		         		}
+		         		$scope.add = function(){
+		         			if($scope.stock != undefined && $scope.stock.symbol != '') {
+		         				stockService.addStockToPortfolio($scope.stock.symbol)
+		         				.then(function(response){
+		         					$scope.stock.error    = null;
+                                    $scope.stock.message  = response.data.response;
+                                    $scope.stock.name     = null;
+                                    $scope.ticker         = null;
+                                    $('#stock-list').load('my_portfolio.js');
+
+		         				},
+		         				 function(response){
+		         				 	$scope.stock = {};
+                                    $scope.stock.error = response.data.response;
+		         				 });
+		         			} else {
+		         				$scope.stock.error = "stock cannot be added";
+		         			}
 		         		}	
 		         	}
 		         }])
